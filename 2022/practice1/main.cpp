3802a8fc8554a4b966fc4985513969f10ce0f219
astronomax <fgfgfb93@gmail.com>
2022-09-14 15:28:58 -0400

    Fix practice 1

--- 2022/practice1/main.cpp
+++ 2022/practice1/main.cpp
@@ -12,8 +12,8 @@
 #include <string_view>
 #include <stdexcept>
 #include <iostream>
+#include <memory>
 
-<<<<<<< HEAD
 std::string to_string(std::string_view str) {
     return std::string(str.begin(), str.end());
 }
@@ -29,35 +29,34 @@ void glew_fail(std::string_view message, GLenum error) {
 GLuint create_shader(GLenum shader_type, const GLchar *shader_source) {
     GLuint shader = glCreateShader(shader_type);
     if (!shader) throw std::runtime_error("fail!");
-    GLint lengths[] = {(GLint) strlen(shader_source)};
-    glShaderSource(shader, 1, &shader_source, lengths);
+    glShaderSource(shader, 1, &shader_source, nullptr);
     glCompileShader(shader);
-    GLint res;
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &res);
-    if (!res) {
-        GLsizei log_len;
-        GLchar log[1024];
-        glGetShaderInfoLog(shader, 1024, &log_len, log);
-        throw std::runtime_error(log);
+    GLint status;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (!status) {
+        GLint length;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
+        std::unique_ptr<char[]> log(new char[length]);
+        glGetShaderInfoLog(shader, length, nullptr, log.get());
+        throw std::runtime_error(log.get());
     }
     return shader;
 }
 
 GLuint create_program(GLuint vertex_shader, GLuint fragment_shader) {
     GLuint program = glCreateProgram();
+    if (!program) throw std::runtime_error("fail!");
     glAttachShader(program, vertex_shader);
     glAttachShader(program, fragment_shader);
     glLinkProgram(program);
-    GLint res;
-    glGetProgramiv(program, GL_LINK_STATUS, &res);
-    if (!res) {
+    GLint status;
+    glGetProgramiv(program, GL_LINK_STATUS, &status);
+    if (!status) {
         GLint length;
         glGetProgramiv(program, GL_INFO_LOG_LENGTH, &length);
-        auto *log = new GLchar[length];
-        glGetProgramInfoLog(program, length, nullptr, log);
-        std::runtime_error exception = std::runtime_error(log);
-        delete [] log;
-        throw exception;
+        std::unique_ptr<char[]> log(new char[length]);
+        glGetProgramInfoLog(program, length, nullptr, log.get());
+        throw std::runtime_error(log.get());
     }
     return program;
 }
@@ -159,78 +158,8 @@ int main() try {
     SDL_GL_DeleteContext(gl_context);
     SDL_DestroyWindow(window);
 }
-catch (std::exception const &e) {
-=======
-std::string to_string(std::string_view str)
-{
-    return std::string(str.begin(), str.end());
-}
-
-void sdl2_fail(std::string_view message)
-{
-    throw std::runtime_error(to_string(message) + SDL_GetError());
-}
-
-void glew_fail(std::string_view message, GLenum error)
-{
-    throw std::runtime_error(to_string(message) + reinterpret_cast<const char *>(glewGetErrorString(error)));
-}
-
-int main() try
-{
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
-        sdl2_fail("SDL_Init: ");
-
-    SDL_Window * window = SDL_CreateWindow("Graphics course practice 1",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        800, 600,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_MAXIMIZED);
-
-    if (!window)
-        sdl2_fail("SDL_CreateWindow: ");
-
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-
-    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-    if (!gl_context)
-        sdl2_fail("SDL_GL_CreateContext: ");
-
-    if (auto result = glewInit(); result != GLEW_NO_ERROR)
-        glew_fail("glewInit: ", result);
-
-    if (!GLEW_VERSION_3_3)
-        throw std::runtime_error("OpenGL 3.3 is not supported");
-
-    glClearColor(0.8f, 0.8f, 1.f, 0.f);
-
-    bool running = true;
-    while (running)
-    {
-        for (SDL_Event event; SDL_PollEvent(&event);) switch (event.type)
-        {
-        case SDL_QUIT:
-            running = false;
-            break;
-        }
-
-        if (!running)
-            break;
-
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        SDL_GL_SwapWindow(window);
-    }
-
-    SDL_GL_DeleteContext(gl_context);
-    SDL_DestroyWindow(window);
-}
 catch (std::exception const & e)
 {
->>>>>>> upstream/master
     std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
 }
